e84edfb751380d6ac515a0d203916fed2185e9e9
Ian <isford7@gmail.com>
2021-06-28 14:24:48 -0500

    Got a hardcoded graph on details page

--- src/components/PenguinDetails/PenguinDetails.jsx
+++ src/components/PenguinDetails/PenguinDetails.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useHistory } from 'react-router';
+import { Line } from 'react-chartjs-2';
 
 export default function PenguinDetails() {
     const history = useHistory();
@@ -22,9 +23,42 @@ export default function PenguinDetails() {
     return (
         <div>
             <p>You are on penguin details page</p>
+            <ul>
+                <li>Name: Nadine</li>
+                <li>Sex: F</li>
+                <li>Mate: Sphen</li>
+                <li>Band Color: Blue/Pink</li>
+                <li>Itraconazole: Not Needed</li>
+                <li>Nesting: None</li>
+                <li>Breeding: None</li>
+                <li>Notes: A good water chicken</li>
+            </ul>
+
+            <div>
+                <Line
+                    data={{
+                        labels: ['6/26', '6/27', '6/28', '6/29', '6/30', '7/1'],
+                        datasets: [
+                            {
+                                label: '# of Fish',
+                                data: [12, 22, 4, 15, 11, 16],
+                                backgroundColor: 'blue',
+                                borderColor: 'blue',
+                                tension: 0.2,
+                            }
+                        ]
+                    }}
+                    height={400}
+                    width={600}
+                    options={{
+                        maintainAspectRatio: true
+                    }}
+                />
+            </div>
+            
             <button onClick={handleGoBack}>Go Back To List</button>
-            <button onClick = {handleEdit}>Edit</button>
-            <button onClick = {handleDelete}>Delete</button>
+            <button onClick={handleEdit}>Edit</button>
+            <button onClick={handleDelete}>Delete</button>
         </div>
     )
 }
